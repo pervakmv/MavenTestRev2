bb443313c92a605660ace4c10dac3186c20fbaa8
Nikolay <pervakmv@elas.kiev.ua>
2018-12-12 15:33:19 +0200

    Delete systems files

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -6,6 +6,6 @@ public class Test {
         System.out.println(IntMath.pow(2,5));
         //Assert.assertTrue(5==10); не можем сделать потому-что указана опция <scope>Test</scope>
         //а это значит, что эта библиотека доступна только в тестах
-        //Комент
+
     }
 }
